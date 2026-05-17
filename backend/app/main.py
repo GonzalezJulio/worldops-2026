3e00457a9ea03e0bc51e1114fca7461ec22441ed
aresden113 <aresden113@gmail.com>
2026-05-17 11:44:45 -0300

    feat: add Redis integration

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from app.redis_client import redis_client
+
+
 
 app = FastAPI()
 
+@app.get("/redis-check")
+def redis_check():
+
+    redis_client.set("test", "worldops")
+
+    value = redis_client.get("test")
+
+    return {
+        "redis": "connected",
+        "value": value
+    }
+    
 
 @app.get("/health")
 def health():
